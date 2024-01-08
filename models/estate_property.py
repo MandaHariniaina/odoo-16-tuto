1eeb4aae84a62393cedc3de377f057cb824e6410
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-01-08 11:48:28 +0300

    Creation form estate property

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from dateutil.relativedelta import relativedelta
+
 from odoo import fields, models, api
 
 
@@ -7,23 +9,35 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate property description'
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string=u"Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
+    date_availability = fields.Date(copy=False, 
+        default=lambda self: fields.datetime.now() + relativedelta(months=3), 
+        string=u"Available From")
+    expected_price = fields.Float(required=True, readonly=True, copy=False)
     selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string=u"Living Area(sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         [
-            ('north',  'North'),
-            ('south',  'South'),
-            ('east',  'East'),
-            ('west',  'West'),
+            ('north',  u'North'),
+            ('south',  u'South'),
+            ('east',  u'East'),
+            ('west',  u'West'),
         ]
     )
+    active = fields.Boolean(default=True)
+
+    STATES = [
+        ('new', u'New'),
+        ('offer_received', u'Offer Received'),
+        ('offer_accepted', u'Offer Accepted'),
+        ('sold', u'Sold'),
+        ('canceled', u'Canceled'),
+    ]
+    state = fields.Selection(STATES, default='new')
