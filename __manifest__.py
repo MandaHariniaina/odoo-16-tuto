6a59936d5bcf1e1dcdf47c560cda18d03e4c7536
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-01-08 10:44:18 +0300

    Create model estate property along with its rules

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,14 @@
 {
     'name': 'Estate',
     'version': '0.1',
+    'category': 'Sales',
+    'description': "Module estate",
+    'depends': [
+        "base"
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
 }
